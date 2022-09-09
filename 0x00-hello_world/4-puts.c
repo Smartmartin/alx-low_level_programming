eb0fa83f331b59bb4cbf959b1ee532b8ffce3f65
Smartmartin <smart4martin@gmail.com>
2022-09-09 00:00:56 -0700

    hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
-4-puts.c
-#include <stdio.h>
+#include <studio.h>
+
 /**
- *  * main - main block
- *   * Return: 0
- *    */
+ * *main - Entry point
+ * *
+ * *Return: Always 0
+ * */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
 }
