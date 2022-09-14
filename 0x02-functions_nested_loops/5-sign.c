e43f3615643c62be61e285f73518cbd320958376
Smartmartin <smart4martin@gmail.com>
2022-09-13 17:25:10 -0700

    functions nested loops

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h" 
+
+ /** 
+  *   * print_sign - print sign of a number 
+  *    * @n: int number 
+  *     * Return: 1 if positive, 0 if 0, -1 if negative 
+  *     */
+int print_sign(int n)
+{
+        if (n > 0)
+        {
+                _putchar('+');
+                return (1);
+        }
+        if (n == 0)
+        {
+                _putchar('0');
+                return (0);
+        }
+        if (n < 0)
+        {
+                _putchar('-');
+                return (-1);
+        }
+        return (2);
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "main.h" 
+
+ /** 
+  *   * print_last_digit - print the last digit of a number 
+  *    * @n: number 
+  *     * Return: last digit of n 
+  *      */
+int print_last_digit(int n)
+{
+        int ldigit;
+        int mask;
+        if (n < 0)
+        {
+                mask = n >> (sizeof(int) * 8 - 1);
+                mask = (mask + n) ^ mask;
+                n = mask + 1;
+        }
+        ldigit = n % 10;
+        _putchar(ldigit + 48);
+        return (ldigit);
+}
