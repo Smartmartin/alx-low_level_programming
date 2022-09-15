8874a04d36621d0de005b94202174591101a4782
Smartmartin <smart4martin@gmail.com>
2022-09-15 05:34:46 -0700

    more functions nested loops

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+/** 
+ * print_numbers - print digits 0-9 
+ * Return: void 
+ */ 
+  
+ void print_numbers(void) 
+{ 
+	int i; 
+	for (i = 48; i <= 57; i++) 
+	{ 
+		_putchar(i); 
+	} 
+	_putchar(10); 
+	return; 
+}
