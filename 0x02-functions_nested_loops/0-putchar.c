10fd42ad5cf8353a74927b79c8903a4299c6660b
Smartmartin <smart4martin@gmail.com>
2022-09-15 12:07:58 -0700

    functions nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,7 +2,8 @@
 /**
   * main - print putchar    
   * Return: always 0      
-  **/
+  *
+  */
 int main(void)
 {
         _putchar('_');
@@ -14,5 +15,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('/n');
-	return (0);
+	return(0);
 }
