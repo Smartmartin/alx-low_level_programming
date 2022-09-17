b8352f993186f1e028262d1b4ca56ec639edd0f6
Smartmartin <smart4martin@gmail.com>
2022-09-17 08:45:41 -0700

    more functions nested loops

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,11 +1,16 @@
-/** 
- * _isdigit - checke whether argument is a digit 
- * @c: int ascii code 
- * Return: int 1 if c is a digit else 
- */ 
- int _isdigit(int c) 
-{ 
-	if ((c >= 48) && (c <= 57)) 
-	return (1); 
-	return (0); 
+/**
+* _isdigit - checke whether argument is a digit
+* @c: int ascii code
+* Return: int 1 if c is a digit else
+*/
+int _isdigit(int c)
+{
+	if ((c >= 48) 
+	&&
+	 
+	(c <= 57))
+	(
+	return (1);
+	return (0);
+	)
 }
--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,10 +1,11 @@
-/** 
- * mul - multiply two ints 
- * @a: int 
- * @b: int 
- * Return: int products of a and b 
- */ 
- int mul(int a, int b) 
-{ 
-	return (a * b); 
+/**
+* mul - multiply two ints
+* @a: int
+* @b: int
+* Return: int products of a and b
+*/
+int mul(int a,	int b)
+{
+	{return (a * b);
+	}
 }
