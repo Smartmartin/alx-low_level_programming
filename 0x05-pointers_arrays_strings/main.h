81760b50098dac93962e01cce129b2e37384e413
Smartmartin <smart4martin@gmail.com>
2022-09-19 21:05:58 -0700

    pointers arrays strings

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,11 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+/**
+ * void prototypes()
+ * int prototypes()
+ */
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -14,4 +19,4 @@ char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 int _putchar(char c);
 
-#endif
+#endif /*MAIN_H*/
