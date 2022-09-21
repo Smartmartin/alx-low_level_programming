2710fd713a47af2c94dd59164e7c879f746b4a65
Smartmartin <smart4martin@gmail.com>
2022-09-21 14:13:26 -0700

    pointers arrays strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+*strcmp - compares two strings.
+*@s1: frist string.
+*@s2: second string.
+*Return: 0 if s1 and s2 are equal,
+*another number if they re not.
+*/
+int _strcmp(char *s1, char *s2)
+{
+int i = 0, op = 0;
+
+while (op == 0)
+{
+if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
+break;
+op = *(s2 + 1) - *(s2 + 1);
+i++;
+}
+return (op);
+}
---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *reverse_array - reverse the contentof an array.
+ *@a: array.
+ *@n: number of elements.
+ *Return: no return.
+ */
+void reverse_array(int *a, int n)
+{
+	int s, d, temp;
+
+	for (s = 0; s < n - 1; s++)
+	{
+		for (d = s + 1; d > 0; d--)
+		{
+			temp = *(a + d);
+			*(a + d) = *(a + (d - 1));
+			*(a + (d - 1)) = temp;
+		}
+	}
+}
